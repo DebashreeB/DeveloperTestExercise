5b33f99add9d5c7c7df1f3c744cf0061cf6e0fc3
DebashreeB <51891614+DebashreeB@users.noreply.github.com>
2019-06-17 14:48:27 +0530

    Update Program.cs

--- FileData/Program.cs
+++ FileData/Program.cs
@@ -9,6 +9,32 @@ namespace FileData
     {
         public static void Main(string[] args)
         {
+            FileDetails flD = new FileDetails();
+            do
+            {
+                string s1;
+
+                s1 = Console.ReadLine().ToString().Trim();
+
+                if (s1.Equals("-v") || s1.Equals("--v") || s1.Equals("/v") || s1.Equals("--version"))
+                {
+                    string strVer = flD.Version("/ThirdPartyTools/FileDates.cs");
+                    Console.WriteLine(strVer);
+                }
+                else if (s1.Equals("-s") || s1.Equals("--s") || s1.Equals("/s") || s1.Equals("--size"))
+                {
+                    int Size = flD.Size("/ThirdPartyTools/FileDates.cs");
+                    Console.WriteLine(Size);
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect response");
+
+                }
+                //System.Threading.Thread.Sleep(30000);
+
+                //Console.ReadLine();
+            } while (!Console.ReadLine().Equals("exit"));
         }
     }
 }
